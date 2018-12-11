ca43d82c64b0cd73014b49dfa58c857ed96cd9c7
Alex <alex.A4.25@yandex.ru>
2018-12-11 13:51:27 +0300

    Added viewer for images

---
+++ lib/views/image_viewer.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ImageViewer extends StatelessWidget {
+  final List<String> _imageUrl;
+
+  ImageViewer(this._imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: Text(_imageUrl.length.toString()),
+      ),
+
+      body: Container(
+        color: Colors.black,
+        child: Image.network(
+          _imageUrl[0],
+          fit: BoxFit.contain,
+        ),
+      ),
+    );
+  }
+}
