a6b5fa2bc2efc38278b36508d00e11ad1f7e572d
Alex <alex.A4.25@yandex.ru>
2018-12-29 16:55:26 +0300

    Added logic to start ImageViewer

--- lib/widgets/clickable_image.dart
+++ lib/widgets/clickable_image.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_yar_zoo/views/image_viewer.dart';
 
 
 
 class ClickableImage extends StatelessWidget {
   final String _imageUrl;
-  final double _width;
 
-  ClickableImage(this._imageUrl, this._width);
+  ClickableImage(this._imageUrl);
 
+  //TODO: change logic of storing data
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      // TODO: add logic to start image-viewer
       onTap: (){
+        Navigator.of(context).push(
+          MaterialPageRoute(
+              builder: (context) => ImageViewer([_imageUrl], 0),
+          ),
+        );
       },
       child: Container(
         padding: EdgeInsets.all(10.0),
@@ -22,7 +27,6 @@ class ClickableImage extends StatelessWidget {
         child: Image.network(
           _imageUrl,
           fit: BoxFit.fitWidth,
-          width: _width,
         ),
       ),
     );
